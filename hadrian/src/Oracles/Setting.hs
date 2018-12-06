cf9767ecafe26ee5e63de6bcb65b6d3268cd4d35
Alec Theriault <alec.theriault@gmail.com>
2018-12-05 16:43:09 -0800

    Hadrian: support extra libraries + dynamic GHC
    Summary:
    This fixes some of the issues that were preventing dynamic GHC from
    being built on OSX.
    
      - Use 'extra-libraries' from .cabal files instead of hardcoding
        (incorrectly) which packages need which extra libs. Also add
        support for 'extra-lib-dirs'.
    
      - Make sure Hadrian looks in the right places to support both
        plain '<pkg>.buildinfo' and '<pkg>.buildinfo.in' files.
    
      - Make the '-rpath' support more robust across OS's (it previously
        didn't work on OSX and possibly windows either).
    
    Test Plan: ./hadrian/build.sh -c # on osx
    
    Reviewers: angerman, alpmestan, adamse, DavidEichmann, bgamari
    
    Subscribers: rwbarton, carter
    
    GHC Trac Issues: #15990
    
    Differential Revision: https://phabricator.haskell.org/D5409

--- hadrian/src/Oracles/Setting.hs
+++ hadrian/src/Oracles/Setting.hs
@@ -3,7 +3,7 @@ module Oracles.Setting (
     getSettingList,  anyTargetPlatform, anyTargetOs, anyTargetArch, anyHostOs,
     ghcWithInterpreter, ghcEnableTablesNextToCode, useLibFFIForAdjustors,
     ghcCanonVersion, cmdLineLengthLimit, iosHost, osxHost, windowsHost,
-    topDirectory, libsuf
+    hostSupportsRPaths, topDirectory, libsuf
     ) where
 
 import Hadrian.Expression
@@ -165,6 +165,11 @@ iosHost = anyHostOs ["ios"]
 -- | Check whether the host OS setting is set to @"darwin"@.
 osxHost :: Action Bool
 osxHost = anyHostOs ["darwin"]
+
+-- | Check whether the host OS supports the @-rpath@ linker option when
+-- using dynamic linking.
+hostSupportsRPaths :: Action Bool
+hostSupportsRPaths = anyHostOs ["linux", "darwin", "freebsd"]
 
 -- | Check whether the host OS setting is set to @"mingw32"@ or @"cygwin32"@.
 windowsHost :: Action Bool
